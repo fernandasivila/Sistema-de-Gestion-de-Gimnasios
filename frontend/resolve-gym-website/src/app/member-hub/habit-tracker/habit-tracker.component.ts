cce332c60b59bdf97560a7f52795197c0e064f56
MaxiiPzzo <altmagicd@gmail.com>
2024-07-08 13:21:03 -0300

    Habit Tracker y Attendance History

--- frontend/resolve-gym-website/src/app/member-hub/habit-tracker/habit-tracker.component.ts
+++ frontend/resolve-gym-website/src/app/member-hub/habit-tracker/habit-tracker.component.ts
@@ -1,12 +1,111 @@
-import { Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-habit-tracker',
   standalone: true,
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './habit-tracker.component.html',
   styleUrl: './habit-tracker.component.css'
 })
-export class HabitTrackerComponent {
+
+export class HabitTrackerComponent implements OnInit {
+  //registro de asistencias de x miembro
+  attendedDays = [1, 3, 5, 8, 10, 13, 15, 19, 22, 24, 25]
+
+  dayGoals = [1, 3, 5] //Lunes, Miercoles y Viernes
+  workingDate = new Date(2024, 6, 25)
+
+  calendarInformation = {
+    baseDateForCalendar: this.workingDate,
+    startDay: new Date((new Date(this.workingDate)).setDate(1)).getDay(),
+    amountOfDays: 0,
+    calendarDates: new Array<Array<number>>,
+    calendarStates: new Array<string>
+  }
+
+  private buildCalendarWeeks() : any[] {
+    //Construir un array de números de calendario
+    let calendarNumbers = Array(this.calendarInformation.startDay)
+    for(let date = 1; date <= this.calendarInformation.amountOfDays;date++){
+      calendarNumbers.push(date)
+    }
+    //Dividir el array en un array de arrays de 7 elementos, semanas
+    let calendarWeeks :Array<Array<number>> = []
+    for(let weekDay = 0; weekDay < calendarNumbers.length; weekDay += 7){
+      const week = calendarNumbers.slice(weekDay, weekDay+7)
+      calendarWeeks.push(week)
+    }
+    return calendarWeeks
+  }
+
+  private buildCalendarStates() : string[] {
+    let dateStates: string[] = []
+    let auxDay = this.calendarInformation.startDay
+    //Evaluar dias anteriores a la fecha actual
+    for (let auxDate = 1; auxDate < this.workingDate.getDate(); auxDate++) {
+      const isGoalDay = this.dayGoals.includes(((auxDay) % 7))
+      if (this.attendedDays.includes(auxDate)) {
+        if(isGoalDay){
+          dateStates.push('Presente')
+        }
+        else{
+          dateStates.push('Extra')
+        }
+      }
+      else if (isGoalDay) {
+        dateStates.push('Ausente')
+      }
+      else {
+        dateStates.push('')
+      }
+      auxDay++
+    }
+    let isGoalDay = this.dayGoals.includes(((this.workingDate.getDate())) % 7)
+    //Evaluar dia actual
+    if (this.attendedDays.includes(this.workingDate.getDate())) {
+      if(isGoalDay){
+        dateStates.push('Presente')
+      }
+      else{
+        dateStates.push('Extra')
+      }
+    }
+    else if (this.dayGoals.includes(((auxDay) % 7))) {
+      dateStates.push('Neutral')
+    }
+    else {
+      dateStates.push('')
+    }
+    auxDay++
+
+    //Evaluar dias posteriores
+    for (let auxDate = this.workingDate.getDate() + 1; auxDate <= this.calendarInformation.amountOfDays; auxDate++) {
+      if (this.dayGoals.includes(((auxDay) % 7))) {
+        dateStates.push('Neutral')
+      }
+      else {
+        dateStates.push('')
+      }
+      auxDay++
+    }
+    return dateStates
+  }
+
+  private getDaysOfMonth(date: Date): number {
+    const year = date.getFullYear()
+    const month = date.getMonth()
+    return (new Date(year, month, 0)).getDate()
+  }
+
+  ngOnInit(): void {
+    console.log([].constructor(this.calendarInformation.amountOfDays))
+    this.calendarInformation['amountOfDays'] = this.getDaysOfMonth(this.workingDate)
+    this.calendarInformation['calendarStates'] = this.buildCalendarStates()
+    this.calendarInformation['calendarDates'] = this.buildCalendarWeeks()
+    console.log(this.calendarInformation['amountOfDays'])
+    console.log(this.calendarInformation.calendarDates)
+    console.log(this.calendarInformation.calendarStates)
+  }
 
 }
--- frontend/resolve-gym-website/src/app/worker-hub/attendance-history/attendance-history.component.ts
+++ frontend/resolve-gym-website/src/app/worker-hub/attendance-history/attendance-history.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataTablesModule } from 'angular-datatables';
+import { ADTSettings } from 'angular-datatables/src/models/settings';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-attendance-history',
   standalone: true,
-  imports: [],
+  imports: [DataTablesModule],
   templateUrl: './attendance-history.component.html',
   styleUrl: './attendance-history.component.css'
 })
-export class AttendanceHistoryComponent {
+export class AttendanceHistoryComponent implements OnInit {
+  dtOptions: ADTSettings = {}
+  dtTrigger: Subject<ADTSettings> = new Subject<ADTSettings>()
 
+  ngOnInit(): void {
+    this.dtOptions = {
+      language: {
+        url: '/assets/datatable.spanish.json',
+      },
+      columns: [
+        { title: 'Fecha', data: null },
+        { title: 'Hora', data: null },
+        { title: 'Usuario', data: null },
+        { title: 'Nombre Completo', data: null },
+      ],
+      columnDefs: [
+        {
+          targets: [0],
+          orderData: [0, 1]
+        },
+        {
+          targets: [1],
+          orderData: [1, 0]
+        },
+      ]
+    }
+  }
 }
