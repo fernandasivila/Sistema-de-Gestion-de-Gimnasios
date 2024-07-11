0d44773c12d1ec7a0320a8c467b363d2e844a37a
fernandasivila <fernandabsivila@gmail.com>
2024-07-11 15:36:10 -0300

    Correcciones de Asistencia

--- Backend/src/routes/attendanceRecord.js
+++ Backend/src/routes/attendanceRecord.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.get('/', attendanceRecordController.list);
 router.get('/:id', attendanceRecordController.getById);
-router.get('/:member', attendanceRecordController.getByMember);
+router.get('/member/:member', attendanceRecordController.getByMember);
 router.get('/attendance/:memberId/:year/:month', attendanceRecordController.findByMemberAndMonth);
 router.post('/add', attendanceRecordValidation, attendanceRecordController.add);
 router.delete('/:id', attendanceRecordController.delete);
